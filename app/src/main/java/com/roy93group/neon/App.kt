29d2f6ad19db4398bdc738616467f6c6e7da67bc
roy <roy93group@gmail.com>
2023-11-11 16:21:33 +0700

    up

---
+++ app/src/main/java/com/roy93group/neon/App.kt
@@ -0,0 +1,21 @@
+package com.roy93group.neon
+
+import android.app.Application
+
+//TODO firebase
+//TODO rate app
+//TODO more app
+//TODO share app
+//TODO policy
+//TODO keystore
+//TODO applovin ad
+
+//done
+//ic launcher
+//leak canary
+//ad id manifest
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
