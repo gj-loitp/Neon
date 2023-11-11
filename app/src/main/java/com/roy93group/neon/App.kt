e62b636364a9f8cd29592795c329969073137804
roy <roy93group@gmail.com>
2023-11-11 16:23:55 +0700

    up

--- app/src/main/java/com/roy93group/neon/App.kt
+++ app/src/main/java/com/roy93group/neon/App.kt
@@ -7,13 +7,14 @@ import android.app.Application
 //TODO more app
 //TODO share app
 //TODO policy
-//TODO keystore
 //TODO applovin ad
 
 //done
 //ic launcher
 //leak canary
 //ad id manifest
+//keystore
+
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
